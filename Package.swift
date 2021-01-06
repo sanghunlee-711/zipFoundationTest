8a654afd0bb87d407e701ad05db26000fde90358
cloudlee711 <cloudlee711@gmail.com>
2021-01-06 17:08:42 +0900

    Add: zipFoundation dependency

--- Package.swift
+++ Package.swift
@@ -12,6 +12,7 @@ let package = Package(
             targets: ["zipFoundationTest"]),
     ],
     dependencies: [
+        .package(url: "https://github.com/weichsel/ZIPFoundation.git", .upToNextMajor(from: "0.9.0"))
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
     ],
@@ -20,7 +21,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "zipFoundationTest",
-            dependencies: []),
+            dependencies: ["ZIPFoundation"]),
         .testTarget(
             name: "zipFoundationTestTests",
             dependencies: ["zipFoundationTest"]),
